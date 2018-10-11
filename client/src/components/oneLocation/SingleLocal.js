b114e52b5fbb25625eed498364af2ebed1efde24
melissachamilton <melissac.hamilton1@gmail.com>
2018-10-11 11:43:50 -0400

    Redirect works

--- client/src/components/oneLocation/SingleLocal.js
+++ client/src/components/oneLocation/SingleLocal.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import { Route, Redirect } from 'react-router'
 
 export default class SingleLocal extends Component {
   state = {
     singleSite: {},
-    listings: []
+    listings: [],
+    redirect: true
   }
 
 
@@ -21,31 +23,30 @@ export default class SingleLocal extends Component {
     this.fetchLocalInfo()
   }
 
+  // re route to a new page upon deletion
+  handleDelete = async () => {
+    const singleLocal = this.props.match.params.localId
+    await axios.delete(`/api/locations/${singleLocal}`)
+    this.props.history.push('/locations')
+  }
+
+  
 
   render() {
-    const singleSite = this.state.singleSite
-    const listingsArray = this.state.listings.map((eachListing, i) => {
-      return (
-        <div key={i}>
-          
-        </div>
-      )
-    })
-
+    const singleSite = this.state.singleSite 
+  
+  
     return (
       <div>
-        
+
         <h1>Single Local</h1>
         <div>Address: {singleSite.address}</div>
         <div>Baths: {singleSite.baths}</div> 
         <div>Beds: {singleSite.beds}</div>
         <div> Description: {singleSite.description} <button>Update</button></div>
-        
+        <button onClick={this.handleDelete}> Delete</button>
 
-
-
-
-        <button>Delete</button>
+      
       </div>
     )
   }
